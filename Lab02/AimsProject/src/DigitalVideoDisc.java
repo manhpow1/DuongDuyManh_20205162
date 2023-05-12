615df18883b89716e362b9cdd68e4154f2b7525a
manhpow1 <dd.manh02@gmail.com>
2023-05-12 08:37:56 +0700

    week3

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -1,4 +1,6 @@
 public class DigitalVideoDisc {
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
     private String title;
     private String category;
     private String director;
@@ -19,6 +21,12 @@ public class DigitalVideoDisc {
     public int getLength() {
         return length;
     }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public float getCost() {
         return cost;
     }
@@ -29,12 +37,16 @@ public class DigitalVideoDisc {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDiscs++;  
+        id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -42,6 +54,15 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        id = nbDigitalVideoDiscs;
+    }
+    
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
     }
 
+    public static void setNbDigitalVideoDiscs(int nbDigitalVideoDiscs) {
+        DigitalVideoDisc.nbDigitalVideoDiscs = nbDigitalVideoDiscs;
+    }
 }
